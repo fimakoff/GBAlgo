8140c0f78bf2acea64dfb2d8fe78cf72e6d3178f
fimakoff <44555531+fimakoff@users.noreply.github.com>
2019-04-11 14:06:41 +0300

    Add fast pow

--- lesson_5/MainPow.java
+++ lesson_5/MainPow.java
@@ -1,21 +1,42 @@
 package lesson_5;
 
 public class MainPow {
-    static int temp = 1;
     public static void main(String[] args) {
-        System.out.println(powRec(6,5));
+        System.out.println(powRec(6,4));
+        System.out.println(fastPow(6,5));
         System.out.println(powFor(6,5));
     }
 
-    public static int powRec(int num, int step){
+    private static long fastPow(int num, int step) {
+        long insight = num;
+        if (step%2==0) {
+            for (int i = 0; i <= step / 2; i++) {
+                insight *= num;
+            }
+        }
+        else{
+
+            for (int i = 0; i <= (step-1)/2; i++) {
+                insight*=num;
+            }
+            insight*=num;
+
+        }
+        return insight;
+    }
+
+    public static long powRec(long num, int step){
         if (step==1) return num;
         num*= powRec(num,step-1);
         return num;
     }
 
     public static int powFor(int num, int step){
+        int temp = 1;
         for (int i = 0; i < step; i++) temp *= num;
         return temp;
     }
+
+
 }
 
