33bbc88de5ce8f850822357b25a497b38aad6568
fimakoff <44555531+fimakoff@users.noreply.github.com>
2019-04-12 16:22:34 +0300

    Update reserve method
    corrected according to the task. 
    the string is changed without using the built-in methods

--- ReserveMain.java
+++ ReserveMain.java
@@ -1,20 +1,12 @@
 package lesson_3;
 
-public class ReserveMain {
+public class MainReserve {
     public static void main(String[] args) {
         String str = ("Java Language is most popular!");
-        reserve(str);
-
-    }
-
-    private static void reserve(String str) {
-        //System.out.println(str);
-        String temp= str;
-        for (int i = str.length()-1; i >=0 ; i--) {
-            temp+= temp.charAt(i);
+        System.out.println("Input string: " + str);
+        MyArrayStack<Character> arr=new MyArrayStack<>();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            arr.push(str.charAt(i));
         }
-        temp = temp.substring(str.length());
-        System.out.println(temp);
+        System.out.println("Output string: " + arr);
     }
-
-}
