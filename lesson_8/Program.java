1ccd41f0c8f897fa1a20be24a25946f91141c4af
fimakoff <44555531+fimakoff@users.noreply.github.com>
2019-04-21 15:14:53 +0300

    Add files
    with resize method

---
+++ lesson_8/Program.java
@@ -0,0 +1,23 @@
+package lesson_8;
+
+public class Program {
+    public static void main(String[] args) {
+        MyChainingHashMap<Character, Integer> hm = new MyChainingHashMap<>();
+        hm.put('a', 2);
+        hm.put('x', 4);
+        hm.put('r', 6);
+        hm.put('c', 12);
+        hm.put('d', 8);
+        hm.put('u', 9);
+        hm.put('t', 19);
+        hm.put('p', 1);
+        hm.put('w', 2);
+        hm.put('q', 4);
+        hm.put('g', 6);
+        hm.put('y', 12);
+        hm.put('j', 8);
+        hm.put('d', 9);
+        hm.put('p', 19);
+        hm.put('e', 1);
+    }
+}
